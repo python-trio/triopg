1551652f06848c84763663f1868e7e8626eef3c3
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-07-28 16:58:25 +0200

    Add exceptions republishing

--- triopg/__init__.py
+++ triopg/__init__.py
@@ -2,5 +2,6 @@
 
 from ._version import __version__
 from ._triopg import connect, create_pool
+from .exceptions import *  # NOQA
 
-__all__ = ('__version__', 'connect', 'create_pool')
+__all__ = ('__version__', 'connect', 'create_pool') + exceptions.__all__ # NOQA
---
+++ triopg/exceptions.py
@@ -0,0 +1,2 @@
+from asyncpg.exceptions import *  # NOQA
+from asyncpg.exceptions import __all__  # NOQA
